da6b074a53816b477d6d89b3312035b5a25127b9
hanthom <m.hantho@gmail.com>
2015-10-26 16:15:06 -0500

    Three more problems solved

--- arraysPractice.js
+++ arraysPractice.js
@@ -153,6 +153,19 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
   //Code Here
+function removeItem(list, item) {
+	for (var i = 0; i < list.length; i++) {
+		if (list[i] === item) {
+			delete list[i];
+		}
+	}
+	return list;
+}
+
+function addItem(list, item) {
+	return list.push(item);
+}
+
 
 //removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
@@ -166,9 +179,15 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 
   //Code Here
+function maker() {
+	var newArray = [];
+	for (var i = 1; i < 216; i++) {
+		newArray[i] = i;
+	}
+	return newArray;
+}
 
-
-
+console.log(maker());
 //Next Problem
 
 
@@ -177,8 +196,14 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 //array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
 
   //Code Here
-
-
+function addTen(nums) {
+	var  newNums = [];
+	for (var i = 0; i < nums.length; i++) {
+		newNums[i] = Number(nums[i]) + 10;
+	}
+	return newNums;
+}
+console.log(addTen(numbers));
 
 //Next Problem
 
